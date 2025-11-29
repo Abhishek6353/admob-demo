1f5916433ecefcc7c96119a68a4bf835b12619ec
Abhishek Kurmi <itsabhi814@gmail.com>
2025-11-30 00:12:33 +0530

    Initial Commit

---
+++ admob-demo/admob_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  admob_demoApp.swift
+//  admob-demo
+//
+//  Created by Abhishek on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct admob_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
